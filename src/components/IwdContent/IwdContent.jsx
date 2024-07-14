726d2c4fc4c41c754a46cd8b4e5c6ee9c2da7d12
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-07-14 20:40:51 +0100

    fixed RUWOFI page issues

--- src/components/IwdContent/IwdContent.jsx
+++ src/components/IwdContent/IwdContent.jsx
@@ -9,7 +9,7 @@ const IwdContent = () => {
     <div className="max-w-[1140px] w-full mx-auto  font-poppins px-5 mt-[70px]">
 <div>
   <h1 className="md:text-[36px] text-[20px] font-semibold text-center">
-  Rural Women Farmers Initiative
+  Rural Women Farmers Initiative (RUWOFI)
   </h1>
 </div>
 
@@ -22,13 +22,7 @@ const IwdContent = () => {
 
 
 <div>
-<p className="md:text-[20px] text-[17px] font-normal md:pt-[100px] pt-[50px]">Kachem Young initiative and Initiative for National Growth designed a yearly conference to commemorate International Women’s Day, and often create a coalition for execution. This conference targets women with disabilities, to increase their access to all levels and forms of education in political and public life, technology, and entrepreneurship. 
-<br/><br/>
-
-The convention supports and fosters women’s economic and civic participation in their respective communities by exposing them to skills and knowledge that will cushion the effects encountered during the COVID-19 period and beyond, and address some cultural impediments to their economic growth.
-<br/><br/>
-
-The convention is designed to strengthen the capacity-building efforts that should pay particular attention to the needs of women to ensure that they are equipped for leadership and decision-making at all levels for all women respective of their disability.
+<p className="md:text-[20px] text-[17px] font-normal md:pt-[100px] pt-[50px]">RUWOFI is an initiative that establishes sustainable agricultural enterprises led by rural farmers under the organisations registered co-operatives to empower rural women financially and to give them economic edge while solving the problem of food insecurity.
 <br/>
 <br/>
 </p>
